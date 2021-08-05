107c1c08dcfc6ea742ce6b5b8a62765c4cf7ebc0
helloworld <hfggggg@gmail.com>
2021-08-05 08:19:42 +0800

    uupdate

--- android/src/main/java/com/reactlibrary/appleauth/AppleAuthenticationModule.java
+++ android/src/main/java/com/reactlibrary/appleauth/AppleAuthenticationModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.reactlibrary.appleauth;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
